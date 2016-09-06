3e565d1d06a0c193ddf475d00233de5d18aee18e
root <root@ip-10-185-173-8.ec2.internal>
2016-09-06 04:06:51 +0000

    existing site

---
+++ includes/jquery.contact.js
@@ -0,0 +1,51 @@
+
+
+
+
+
+/*
+     FILE ARCHIVED ON 9:29:13 Aug 3, 2015 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 5:11:33 Dec 29, 2015.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+
+(function($) { 
+	"use strict";
+	jQuery(document).ready(function(){
+	$('#cform').submit(function(){
+
+		var action = $(this).attr('action');
+
+		$("#message").slideUp(750,function() {
+		$('#message').hide();
+
+ 		$('#submit')
+			.before('<img src="images/ajax-loader.gif" class="contact-loader" />')
+			.attr('disabled','disabled');
+
+		$.post(action, {
+			name: $('#name').val(),
+			email: $('#email').val(),
+			comments: $('#comments').val(),
+		},
+			function(data){
+				document.getElementById('message').innerHTML = data;
+				$('#message').slideDown('slow');
+				$('#cform img.contact-loader').fadeOut('slow',function(){$(this).remove()});
+				$('#submit').removeAttr('disabled');
+				if(data.match('success') != null) $('#cform').slideUp('slow');
+			}
+		);
+
+		});
+
+		return false;
+
+	});
+
+});
+	
+	}(jQuery));
